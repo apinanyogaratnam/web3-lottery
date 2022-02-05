ef05c035513805724098f78639d9d8afa6e5155f
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-05 10:09:42 -0500

    refactor: using mumbai rpc url for web3 instance

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -7,7 +7,7 @@ if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     web3 = new Web3(window.ethereum);
 } else {
     const provider = new Web3.providers.HttpProvider(
-        "https://rinkeby.infura.io/v3/d55c4f57b80445d284aa7754f355aaeb"
+        `https://rpc-mumbai.maticvigil.com/v1/${process.env.URL}`
     );
     web3 = new Web3(provider);
 }
