4cffc136e6297e18ea0f2d22e11c0ad2f332ffe3
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-05 09:11:30 -0500

    refactor: completed lottery contract

---
+++ ethereum/contracts/Lottery.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Lottery {
+    
+    address ownerAddress;
+    uint jackpotAmount;
+    mapping (address => uint) playersToAmountDeposited;
+    address[] players;
+
+    mapping(address => uint) winnersHistory;
+    
+    constructor() {
+        ownerAddress = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == ownerAddress);
+        _;
+    }
+
+    function depositIntoJackpot() external payable {
+        jackpotAmount += msg.value;
+
+        address player = msg.sender;
+        if (playersToAmountDeposited[player] == 0) {
+            players.push(player);
+            playersToAmountDeposited[player] = msg.value;
+
+            return;
+        }
+
+        playersToAmountDeposited[player] += msg.value;
+    }
+
+    function getJackpotAmount() external view returns (uint) {
+        return jackpotAmount;
+    }
+
+    function random() internal view returns (uint) {
+        return uint(keccak256(abi.encode(block.difficulty, block.timestamp, players)));
+    }
+
+    function resetPlayersToAmountDeposited() internal {
+        for (uint i=0; i<players.length; i++) {
+            playersToAmountDeposited[players[i]] = 0;
+        }
+    }
+
+    function drawLottery() onlyOwner external returns (address) {
+        uint index = random() % players.length;
+        address winner = address(players[index]);
+        uint contractBalance = address(this).balance;
+
+        payable(winner).transfer(contractBalance);
+        winnersHistory[winner] += contractBalance;
+
+        resetPlayersToAmountDeposited();
+        players = new address[](0);
+
+        return winner;
+    }
+}
