9e9854435e79e8bfb398c2a5beb0dbef25a7d602
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-05 10:58:37 -0500

    refactor: integration with covalent data

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,13 @@ import styles from '../styles/Home.module.css';
 import lottery from '../ethereum/lottery';
 import web3 from '../ethereum/web3';
 import React from 'react';
+import FileSaver from 'file-saver';
 
 export default function Home() {
   const [jackpotTotal, setJackpotTotal] = React.useState('');
   const [amountToStake, setAmountToStake] = React.useState('');
+  const [address, setAddress] = React.useState('');
+  const [covalentData, setCovalentData] = React.useState([]);
 
   React.useEffect(() => {
     async function fetchData() {
@@ -37,15 +40,24 @@ export default function Home() {
     });
   };
 
+  const downloadMetadata = async (event) => {
+    const covalent = "https://api.covalenthq.com/v1/1/address/" + address + "/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=false&key=" + process.env.NEXT_PUBLIC_COVALENT_API_KEY;
+    const covalentRes = await axios.get(covalent);
+    setCovalentData(covalentRes.data.data);
+    FileSaver.saveAs(covalentRes.data.data, "covalent.json");
+  };
+
   return (
     <div>
       <h1>Web3 Lottery</h1>
       <h3>Jackpot Total: {jackpotTotal}</h3>
       <form>
-        <input type="text" placeholder="amount to stake" value={amountToStake} onChange={e => setAmountToStake(e.target.value)} />
+        <input type='text' placeholder='amount to stake' value={amountToStake} onChange={e => setAmountToStake(e.target.value)} />
+        <input type='text' placeholder='address' value={address} onChange={e => setAddress(e.target.value)} />
       </form>
       <button onClick={stakeNow}>Stake</button>
       <button onClick={drawNow}>Draw</button>
+      <button onClick={downloadMetadata}>Download Metadata</button>
     </div>
   )
 }
